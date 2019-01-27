f13e69993c5d37aa0649daf1170496edee72eb2a
Heloise Taillet <heloise96@hotmail.com>
2019-01-27 16:09:57 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Heloise"
+  end
+
+  get '/hometown' do
+    "My hometown is potomac"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Aicha"
+  end
+
 end
