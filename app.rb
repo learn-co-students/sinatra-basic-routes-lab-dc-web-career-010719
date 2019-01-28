6a9c34e66b4b152b8877f5c97d457ecf540d4807
Ben Yellin <byellindc@gmail.com>
2019-01-27 20:15:08 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Ben"
+  end
+
+  get '/hometown' do
+    "My hometown is also Ben, strangely"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is (you guessed it) Ben"
+  end
 end
