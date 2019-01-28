4eefb1f0041fe125865780ccedfc7b9c87bde2ed
James Clement <jameseclement@gmail.com>
2019-01-27 21:33:25 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/' do
+  "Hello, World!"
+end
+
+get '/name' do
+  "My name is James"
+end
+
+get '/hometown' do
+  "My hometown is Fairfax"
+end
+
+get '/favorite-song' do
+  "My favorite song is Forever On Your Side" 
+end
+
+
+
 end
