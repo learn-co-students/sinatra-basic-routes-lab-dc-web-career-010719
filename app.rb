c2cb1965129f4ca1eda11eea0e25e3bf595037e7
Dusty211 <kylebenz@hotmail.com>
2019-01-28 15:56:10 -0500

    All tests pass.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Kyle"
+  end
+
+  get '/hometown' do
+    "My hometown is Hometown"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Song That Never Ends"
+  end
+
 end
